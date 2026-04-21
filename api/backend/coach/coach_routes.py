fa4c04c81f20dbba0176091cf51582f7c289b70e
TannopTang <tangpiroonthum.t@northeastern.edu>
2026-04-20 20:31:44 -0400

    Update coach_routes.py

--- api/backend/coach/coach_routes.py
+++ api/backend/coach/coach_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, current_app, ResponseReturnValue
+from flask import Blueprint, jsonify, request, current_app
 from backend.db_connection import get_db
 from mysql.connector import Error
 
